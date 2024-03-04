da2ed5c53b7995e8bba10396dc58df793bf084e3
Quickrice <162115830+Quickrice@users.noreply.github.com>
2024-03-04 14:55:30 +0100

    Update Sanguinius_Arts.py
    Didnt work so i hope this one works

--- Sanguinius_Arts.py
+++ Sanguinius_Arts.py
@@ -5,24 +5,17 @@ def main():
     st.title('Sanguis')
     st.header('Mysterium Obscurum: Sanguis Est Via Ad Potentiam')
     st.subheader('Ritus invocote:')
-    st.code('streamlit run Ritus Sanguinis.py')
+    st.code('streamlit run Ritus_Sanguinis.py')
     st.markdown("*Occult* is **really** ***hot***.")
     st.markdown('''
     :red[Sanguis est via ad potentiam, arcanum magicae].''')
-    
 
     # Function to get user input for percentage of each component
     def get_user_input(label, default_value=50):
-        return st.slider(f"percentagium de {label} (%)", min_value=0, max_value=100, value=default_value)
+        return st.slider(f"Percentagium de {label} (%)", min_value=0, max_value=100, value=default_value)
 
     # Pre-set default percentages
     default_values = {'Erythrocytae': 40, 'Leukocytae': 1, 'Thrombocytae': 1, 'Plasma': 58}
-
-    # Sidebar inputs
-    st.sidebar.title('recede')
-    with st.sidebar:
-        st.subheader('Adhuc hic? Abi!')
-       
 
     # Get user input for each component
     erythrocytae_percent = get_user_input('Erythrocytae', default_values['Erythrocytae'])
@@ -37,10 +30,11 @@ def main():
     if total_percent != 100:
         st.warning("Total percentage does not equal 100. Adjusting values to correct.")
         # Adjust values to make total 100
-        erythrocytae_percent = erythrocytae_percent / total_percent * 100
-        leukocytae_percent = leukocytae_percent / total_percent * 100
-        thrombocytae_percent = thrombocytae_percent / total_percent * 100
-        plasma_percent = plasma_percent / total_percent * 100
+        factor = 100 / total_percent
+        erythrocytae_percent *= factor
+        leukocytae_percent *= factor
+        thrombocytae_percent *= factor
+        plasma_percent *= factor
 
     # Sample data
     labels = ['Erythrocytae', 'Leukocytae', 'Thrombocytae', 'Plasma']
@@ -56,16 +50,16 @@ def main():
     fig.update_traces(marker=dict(colors=['#FF5733', '#FF8C33', '#FFBB33', '#FFD433']))
 
     # Set layout title
-    fig.update_layout(title='elementa hematocriti')
+    fig.update_layout(title='Elementa Hematocriti')
 
     # Render the pie chart
     st.plotly_chart(fig)
 
     option = st.selectbox(
-    'Intellegisne sanguinem tuum?',
-    ('ita', 'non', 'nescio'))
-
-    st.write('tu elegisti:', option)
+        'Intellegisne sanguinem tuum?',
+        ('Ita', 'Non', 'Nescio')
+    )
+    st.write('Tu elegisti:', option)
 
 if __name__ == '__main__':
     main()
